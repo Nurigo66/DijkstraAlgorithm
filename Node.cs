eb215a44df868fa917ac710ad0abaca3bf2600c4
Nurigo66 <162607187+Nurigo66@users.noreply.github.com>
2024-03-19 17:12:28 +0300

    Initial commit

---
+++ Node.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DijkstraAlgorithm
+{
+    internal class Node
+    {
+        public string Name { get; set; }
+        public Dictionary<Node, int> Edges { get; set; }
+
+        public Node(string name)
+        {
+            Name = name;
+        }
+        public Node(string name, Dictionary<Node, int> edges)
+        {
+            Name = name;
+            Edges = edges;
+        }
+        public Node AddEdge(Node node,int distance)
+        {
+            if(Edges==null)
+                Edges = new Dictionary<Node, int>();
+            Edges.Add(node, distance);
+            return this;
+        }
+
+        public override string ToString() => $"Nodes:{Name}, Edges  {Edges?.Count}  ";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using DijkstraAlgorithm;
+
+var nodes = new Dictionary<string, Node>()
+{
+    ["A"] = new Node("A"),
+    ["B"] = new Node("B"),
+    ["C"] = new Node("C"),
+    ["D"] = new Node("D"),
+    ["E"] = new Node("E"),
+    ["F"] = new Node("F")
+};
+nodes["A"].AddEdge(nodes["B"], 2).AddEdge(nodes["D"], 8);
+nodes["B"].AddEdge(nodes["A"], 2).AddEdge(nodes["D"], 5).AddEdge(nodes["E"], 6);
+nodes["C"].AddEdge(nodes["E"], 9).AddEdge(nodes["F"], 3);
+nodes["D"].AddEdge(nodes["A"], 8).AddEdge(nodes["B"], 5).AddEdge(nodes["E"], 3).AddEdge(nodes["F"], 2);
+nodes["E"].AddEdge(nodes["B"], 6).AddEdge(nodes["C"], 9).AddEdge(nodes["F"], 1).AddEdge(nodes["D"], 3);
+nodes["F"].AddEdge(nodes["D"], 2).AddEdge(nodes["E"], 1).AddEdge(nodes["C"], 3);
+
+var finalNode = new Node("C");
+
+
+var distances = nodes.ToDictionary(kvp => kvp.Value, kvp => (int?)int.MaxValue);
+var parent = new Dictionary<Node, Node>();
+var undiscoveredNodes = new HashSet<Node>(nodes.Values);
+
+distances[nodes["A"]] = 0;
+
+while (undiscoveredNodes.Count > 0)
+{
+    var current = undiscoveredNodes.MinBy(node => distances[node]);
+    undiscoveredNodes.Remove(current);
+
+    if (current == finalNode) { break; }
+
+    foreach (var (adjacentNode, distance) in current.Edges)
+    {
+        var subDistance = distances[current] + distance;
+        if (subDistance < distances[adjacentNode])
+        {
+            distances[adjacentNode] = subDistance;
+            parent[adjacentNode] = current;
+
+        }
+
+    }
+
+}
+
+var pathNodes = new List<Node>();
+var currentNode = finalNode;
+
+while (currentNode != null)
+{
+    pathNodes.Insert(0, currentNode);
+    currentNode = parent.TryGetValue(currentNode, out var parentNode) ? parentNode : null;
+}
+Console.WriteLine(string.Join("=>", pathNodes.Select(i => i.Name)));
+        Console.WriteLine($"Total Distance = {distances[finalNode]}");
+    
+
+
+
